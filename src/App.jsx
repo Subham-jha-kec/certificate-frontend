339e33573d6076b5728d27aee09a6c9e5b9bf791
Subham Kumar Jha <subhamgdsc@gmail.com>
2025-09-27 23:31:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,10 @@ function App() {
 
 
       // const API_URL = import.meta.env.VITE_API_URL;
-      const response = await fetch(`https://certificate-backend-9ksc.onrender.com/distribute-files`, {
+      // const response = await fetch(`https://certificate-backend-9ksc.onrender.com/distribute-files`, {
+      // method: 'POST',
+      // body: formData,
+       const response = await fetch(`https://certificate-backend-iota.vercel.app`, {
       method: 'POST',
       body: formData,
 });
